c932090350bf5c70270679f0c5852f59478da3b5
AndreasOberascher <andreas.oberascher@utilo.eu>
2015-09-08 13:39:57 +0200

    Benutzer anlegen Tabellen entfernen und zu Liste machen

--- rezervi/webinterface/benutzerBearbeiten/benutzerAnlegen.php
+++ rezervi/webinterface/benutzerBearbeiten/benutzerAnlegen.php
@@ -31,54 +31,97 @@ $sprache = getSessionWert(SPRACHE);
 <script language="JavaScript" type="text/javascript" src="./benutzerDaten.php">
 </script>
 <?php include_once("../templates/bodyA.php"); ?>
-<form action="./benutzerEintragen.php" method="post" name="benutzer" id="benutzer" target="_self" onSubmit="return chkFormular()">
+
+<h2><?php echo(getUebersetzung("Benutzer anlegen",$sprache,$link)); ?><br/>      
+<h4><?php echo(getUebersetzung("Bitte füllen Sie die untenstehenden Felder aus.",$sprache,$link)); ?><br/>
+    <?php echo(getUebersetzung("Die mit [*] gekennzeichneten Felder müssen ausgefüllt werden!",$sprache,$link)); ?>
+</h4>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+    <a class="btn btn-primary" href="./index.php"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>&nbsp;<?php echo(getUebersetzung("zurück",$sprache,$link)); ?></a>
+  </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+<form action="./benutzerEintragen.php" method="post" name="benutzer" id="benutzer" target="_self" onSubmit="return chkFormular();" class="form-horizontal">
+			
+<!-- <form action="./benutzerEintragen.php" method="post" name="benutzer" id="benutzer" target="_self" onSubmit="return chkFormular()">
   <table border="0" cellpadding="0" cellspacing="3" class="table">
     <tr>
       <td colspan="2"><p class="standardSchriftBold"><?php echo(getUebersetzung("Benutzer anlegen",$sprache,$link)); ?><br/>
-          <span class="standardSchrift"><?php echo(getUebersetzung("Bitte f�llen Sie die untenstehenden Felder aus.",$sprache,$link)); ?> 
+          <span class="standardSchrift"><?php echo(getUebersetzung("Bitte füllen Sie die untenstehenden Felder aus.",$sprache,$link)); ?> 
           <br/>
-          <?php echo(getUebersetzung("Die mit [*] gekennzeichneten Felder m�ssen ausgef�llt werden!",$sprache,$link)); ?></span></p></td>
-    </tr>
-    <tr>
+          <?php echo(getUebersetzung("Die mit [*] gekennzeichneten Felder müssen ausgefüllt werden!",$sprache,$link)); ?></span></p></td>
+    </tr> -->
+    <!-- <tr>
       <td height="30" colspan="2">&nbsp;</td>
-    </tr>
-    <tr>
+    </tr> -->
+    <!-- <tr>
       <td width="50%"><?php echo(getUebersetzung("Benutzername",$sprache,$link)); ?>*</td>
       <td width="50%"><input name="name" type="text" id="name" value="" maxlength="20"></td>
-    </tr>
-    <tr>
+    </tr> -->
+    <div class="form-group">
+				<label for="name" class="col-sm-2 control-label"><?php echo(getUebersetzung("Benutzername",$sprache,$link)); ?>*</label>
+				<div class="col-sm-10">
+					<input name="name" type="text" id="name" value="" class="form-control">
+				</div>
+			</div>	
+    <!-- <tr>
       <td><?php echo(getUebersetzung("Passwort",$sprache,$link)); ?>*</td>
       <td><input name="pass" type="password" id="pass" value="" maxlength="20"></td>
-    </tr>
-    <tr>
+    </tr> -->
+    <div class="form-group">
+				<label for="pass" class="col-sm-2 control-label"><?php echo(getUebersetzung("Passwort",$sprache,$link)); ?>*</label>
+				<div class="col-sm-10">
+					<input name="pass" type="text" id="pass" value="" class="form-control">
+				</div>
+	</div>
+    <!-- <tr>
       <td><?php echo(getUebersetzung("Passwort wiederholen",$sprache,$link)); ?>*</td>
       <td><input name="pass2" type="password" id="pass2" value="" maxlength="20"></td>
-    </tr>
-    <tr>
+    </tr> -->
+     <div class="form-group">
+				<label for="pass2" class="col-sm-2 control-label"><?php echo(getUebersetzung("Passwort wiederholen",$sprache,$link)); ?>*</label>
+				<div class="col-sm-10">
+					<input name="pass2" type="text" id="pass2" value="" class="form-control">
+				</div>
+	</div>
+    <!-- <tr>
       <td height="30" colspan="2">&nbsp;</td>
-    </tr>
-    <tr>
+    </tr> -->
+    <!-- <tr>
       <td><?php echo(getUebersetzung("Benutzerrechte",$sprache,$link)); ?>*</td>
       <td><select name="rechte">
           <option value="1" selected><?php echo(getUebersetzung("Benutzer",$sprache,$link)); ?></option>
           <option value="2"><?php echo(getUebersetzung("Administrator",$sprache,$link)); ?></option>
         </select></td>
-    </tr>
-    <tr>
-      <td colspan="2"><input name="Submit" type="submit" id="Submit" class="btn btn-success"  
+    </tr> -->
+    <div class="form-group">
+				<label for="rechte" class="col-sm-2 control-label"><?php echo(getUebersetzung("Benutzerrechte",$sprache,$link)); ?>*</label>
+				<div class="col-sm-10">
+	   <select name="rechte" type="text" id="rechte" value="" class="form-control">
+		  <option value="1" selected><?php echo(getUebersetzung("Benutzer",$sprache,$link)); ?></option>
+          <option value="2"><?php echo(getUebersetzung("Administrator",$sprache,$link)); ?>	   </option>
+       </select>
+				</div>
+	</div>
+    
+     <input name="Submit" type="submit" id="Submit" class="btn btn-success"  
       value="<?php echo(getUebersetzung("Benutzer anlegen",$sprache,$link)); ?>">
-     </td>
-    </tr>
-  </table>
+     
+    
+
 </form>
-<table border="0" cellpadding="0" cellspacing="0" class="table">
+<!-- <table border="0" cellpadding="0" cellspacing="0" class="table">
   <tr>
     <td>
     	<a class="btn btn-primary" href="./index.php"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>&nbsp;<?php echo(getUebersetzung("zurück",$sprache,$link)); ?></a>
-    	<!-- <form action="./index.php" method="post" name="zimmer aendern" target="_self" id="zimmer aendern">
+    	<form action="./index.php" method="post" name="zimmer aendern" target="_self" id="zimmer aendern">
         <input name="retour" type="submit" class="button200pxA" id="retour" onMouseOver="this.className='button200pxB';"
 	 onMouseOut="this.className='button200pxA';" value="<?php echo(getUebersetzung("zurück",$sprache,$link)); ?>">
-      </form> -->
+      </form>
     </td>
   </tr>
 </table>
@@ -90,6 +133,6 @@ $sprache = getSessionWert(SPRACHE);
 	 onMouseOut="this.className='button200pxA';" value="<?php echo(getUebersetzung("Hauptmen�",$sprache,$link)); ?>">
       </form></td>
   </tr>
-</table>
+</table> -->
 </body>
 </html>
