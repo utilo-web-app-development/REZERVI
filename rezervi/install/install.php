a3817b620e4b835e0d24cb5638d7fcb13d3d674e
Andreas Oberascher <andreas.oberascher@utilo.eu>
2016-07-13 11:33:13 +0200

    design

--- rezervi/install/install.php
+++ rezervi/install/install.php
@@ -48,7 +48,7 @@ include_once("../include/uebersetzer.php");
   <div class="panel-body">
 
 
-<p class="standardSchriftBold"><?php 
+<b><?php 
 	if ($sprache == "en"){
 		?>
 		Doing the installation ...
@@ -60,14 +60,14 @@ include_once("../include/uebersetzer.php");
 	<?php 
 	}	
 	?>
-</p>
+</b>
 <?php
 	//installation durchfuehren:
 	//1. tabellen:
 	include_once("installTables.php");
 	setSessionWert(SPRACHE,$sprache);
 ?>
-<p class="standardSchrift"><?php 
+<p><?php 
 	if ($sprache == "en"){
 		?>
 		Part 1 finished ...
@@ -84,7 +84,7 @@ include_once("../include/uebersetzer.php");
 	//2. unterkunft:
 	include_once("UKeintragen.php");
 ?>
-<p class="standardSchrift"><?php
+<p><?php
 	if ($sprache == "en"){
 		?>
 		Part 2 finished ...
@@ -100,32 +100,32 @@ include_once("../include/uebersetzer.php");
 	//3. sprache
 	include_once("installSprache.php");
 ?>
-<p class="standardSchrift"><?php echo(getUebersetzung("Teil 3 abgeschlossen",$sprache,$link)); ?></p>
+<p><?php echo(getUebersetzung("Teil 3 abgeschlossen",$sprache,$link)); ?></p>
 
 <?php if ($fehler == true) { ?><p class="belegt"><?php echo($antwort); ?></p><?php } ?>
 
-<p class="standardSchriftBold"><?php echo(getUebersetzung("So können sie Rezervi verlinken",$sprache,$link)); ?>:</p>
+<b><?php echo(getUebersetzung("So können sie Rezervi verlinken",$sprache,$link)); ?>:</b>
 <ul>
-  <li class="standardSchrift"><?php echo(getUebersetzung("Die Startseite Ihres Belegungsplanes",$sprache,$link)); ?>: <br/>
+  <li><?php echo(getUebersetzung("Die Startseite Ihres Belegungsplanes",$sprache,$link)); ?>: <br/>
     <?php echo(getUebersetzung("http://ihre-domain/ihrInstallationsverzeichnis/",$sprache,$link)); ?><a href="../start.php" target="_blank">start.php</a></li>
-  <li class="standardSchrift"><?php echo(getUebersetzung("Dieser Seite können Sie auch die gewünschte Sprache übergeben",$sprache,$link)); ?>:<br/>
+  <li ><?php echo(getUebersetzung("Dieser Seite können Sie auch die gewünschte Sprache übergeben",$sprache,$link)); ?>:<br/>
   <?php echo(getUebersetzung("http://ihre-domain/ihrInstallationsverzeichnis/",$sprache,$link)); ?><a href="../start.php?spracheNeu=de" target="_blank">start.php?spracheNeu=de</a><br/>
   <?php echo(getUebersetzung("http://ihre-domain/ihrInstallationsverzeichnis/",$sprache,$link)); ?><a href="../start.php?spracheNeu=en" target="_blank">start.php?spracheNeu=en</a><br/>
   <?php echo(getUebersetzung("http://ihre-domain/ihrInstallationsverzeichnis/",$sprache,$link)); ?><a href="../start.php?spracheNeu=fr" target="_blank">start.php?spracheNeu=fr</a></li>
-  <li class="standardSchrift"><?php echo(getUebersetzung("Sie können auch direkt die Suchfunktion aufrufen",$sprache,$link)); ?>:<br/>
+  <li><?php echo(getUebersetzung("Sie können auch direkt die Suchfunktion aufrufen",$sprache,$link)); ?>:<br/>
   <?php echo(getUebersetzung("http://ihre-domain/ihrInstallationsverzeichnis/",$sprache,$link)); ?><a href="../suche/index.php" target="_blank">suche/index.php</a></li>
-  <li class="standardSchrift"><?php echo(getUebersetzung("Auch dieser Seite kann die gewünschte Sprache übergeben werden",$sprache,$link)); ?>: <br/>
+  <li><?php echo(getUebersetzung("Auch dieser Seite kann die gewünschte Sprache übergeben werden",$sprache,$link)); ?>: <br/>
   <?php echo(getUebersetzung("http://ihre-domain/ihrInstallationsverzeichnis/",$sprache,$link)); ?><a href="../suche/index.php?sprache=de" target="_blank">suche/index.php?sprache=de</a><br/>
   <?php echo(getUebersetzung("http://ihre-domain/ihrInstallationsverzeichnis/",$sprache,$link)); ?><a href="../suche/index.php?sprache=en" target="_blank">suche/index.php</a><a href="../suche/index.php" target="_blank">?sprache=en</a><br/>
   <?php echo(getUebersetzung("http://ihre-domain/ihrInstallationsverzeichnis/",$sprache,$link)); ?><a href="../suche/index.php?sprache=fr" target="_blank">suche/index.php</a><a href="../suche/index.php" target="_blank">?sprache=fr</a> </li>
-  <li class="standardSchrift"><?php echo(getUebersetzung("Den Zugang zur Webschnittstelle erhalten sie über",$sprache,$link)); ?>
+  <li><?php echo(getUebersetzung("Den Zugang zur Webschnittstelle erhalten sie über",$sprache,$link)); ?>
   (<?php echo(getUebersetzung("Verwenden sie als Benutzername und Passwort \"test\" und ändern sie das Passwort nach erstmaligem Einstieg.",$sprache,$link)); ?>):<br/>
   <?php echo(getUebersetzung("http://ihre-domain/ihrInstallationsverzeichnis/",$sprache,$link)); ?><a href="../webinterface/index.php" target="_blank">webinterface/index.php</a></li>
 </ul>
-<p class="standardSchrift"><?php echo(getUebersetzung("Nach erfolgreicher Installation sollten Sie den Ordner &quot;install&quot; auf Ihrem Webserver löschen, ansonsten kann durch einen Angriff von aussen Ihre Datenbank verändert werden",$sprache,$link)); ?>!</p>
+<p><?php echo(getUebersetzung("Nach erfolgreicher Installation sollten Sie den Ordner &quot;install&quot; auf Ihrem Webserver löschen, ansonsten kann durch einen Angriff von aussen Ihre Datenbank verändert werden",$sprache,$link)); ?>!</p>
 
-<p class="standardSchrift"><?php echo(getUebersetzung("Wir sind ihnen auch gerne bei Fragen zur Installation behilflich. Senden sie uns einfach ein E-Mail unter",$sprache,$link)); ?>:</p>
-<p class="standardSchrift"><a href="mailto:rezervi@utilo.eu">rezervi@utilo.eu</a></p>
+<p><?php echo(getUebersetzung("Wir sind ihnen auch gerne bei Fragen zur Installation behilflich. Senden sie uns einfach ein E-Mail unter",$sprache,$link)); ?>:</p>
+<p><a href="mailto:rezervi@utilo.eu">rezervi@utilo.eu</a></p>
 
 <!-- webinterface öffnen -->
 <form action="../webinterface/index.php" method="post" id="formLizenz" name="formLizenz" target="_self" onSubmit="return checkLicence();">
@@ -133,6 +133,7 @@ include_once("../include/uebersetzer.php");
 	<input name="Submit" type="submit" class="btn btn-primary" value="<?= getUebersetzung("Webinterface öffnen",$sprache,$link) ?>">
 </form>
 <!-- ende webinterface öffnen -->
-
+</div>
+</div>
 </body>
 </html>
